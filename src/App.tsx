0f82bbe4cc3734f72db32e1a1a082e13e2eba819
U-C-S <ucs3721@outlook.com>
2020-10-24 16:24:13 +0530

    intial 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="alll" id="allo">
+    <div className="pagehead">
+      <div className="heading" id="header"><h1 id="title">The BOcT</h1></div>
+      <div className="bot-head">
+        <button className="bot-btn" id="BOcT" title="The BOcT">
+          <svg viewBox="0 0 640 512"><path id="svg0"/></svg>
+        </button>
+      </div>
     </div>
+
+    <div className="arrow_box" id="chatter">
+      <div className="talk_box" id="chatspace"></div>
+      <div className="type_box">
+        <form className="type_box-inner" action="javascript:void(0);">
+          <input className="typespace" id="typespace-id" type="text" placeholder="Wanna talk with BOcT? Then type here..!" autoComplete="off"/>
+          <button className="typespace-enter" id="typespace-enter-id" type="submit">
+            <svg viewBox="0 0 448 512"><path id="svg1"/></svg>
+          </button>
+        </form>
+      </div>
+    </div>
+
+    <div className="about">
+      <fieldset>
+        <legend>About BOcT</legend>
+        <div className="aboutbtns">
+          <button id="settingg" title="Settings">
+            <svg viewBox="0 0 512 512"><path id="svg2"/></svg></button>
+          <button id="i-btn" title="Info">
+            <svg viewBox="0 0 512 512"><path id="svg3"/></svg></button>
+          <button className="toggleTheme" data-themenow="dark" title="Toggle Dark/Light Mode">
+            <svg viewBox="0 0 512 512"><path id="svg4"/></svg></button>
+        </div>
+        <p className="releaseVer">Version 3.1.3 | Oct 2020 Release</p>
+        <hr/>
+        <p>BOcT is a Static Browser Chatbot</p>
+        <p>It's currently in Lazy Development</p>
+        <p>Future Updates to BOcT can make it perform basic Maths, Maybe suggest you some Movies...</p>
+      </fieldset>
+    </div>
+  </div>
+
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,6 @@ window.addEventListener('load', () => { setTimeout(()=>{
 }, 2173) });
 
 
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
