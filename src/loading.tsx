40c7a7c53a634846ad3244d73deb4738692911d4
U-C-S <ucs3721@outlook.com>
2020-10-24 15:41:25 +0530

    Initial Setup 1 - added CSS - added loading screen

---
+++ src/loading.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Load() {
+  return (
+    <div id="loadScreen">
+      <div className="loader">
+        <div id="load_barr"></div>
+        <div id="load_text">BOcT Loading...</div>
+      </div>
+    </div>
+  );
+}
+export default Load;
